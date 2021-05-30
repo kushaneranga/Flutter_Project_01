02a855a6154f44d5781f492ede8ece7e6189fe18
Kushan Eranga <kem.eranga98@gmail.com>
2021-05-30 19:13:38 +0530

    App Bar

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -12,3 +12,35 @@ import 'package:flutter/material.dart';
 //     );
 //   }
 // }
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Bar Title',
+            style: TextStyle(
+              letterSpacing: 2.0,
+              wordSpacing: 4.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Text('My fist flutter project',
+            style: TextStyle(
+                color: Colors.lightBlue,
+                fontSize: 20.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  
+}
