26f474fa8e2f64a9af442688d4d39d3e6ba45c8b
Kushan Eranga <kem.eranga98@gmail.com>
2021-05-30 21:37:31 +0530

    AppBar Test 01

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -13,34 +13,34 @@ import 'package:flutter/material.dart';
 //   }
 // }
 
-void main(){
-  runApp(MyApp());
-}
+// void main(){
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Bar Title',
-            style: TextStyle(
-              letterSpacing: 2.0,
-              wordSpacing: 4.0,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        body: Center(
-          child: Text('My fist flutter project',
-            style: TextStyle(
-                color: Colors.lightBlue,
-                fontSize: 20.0,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Bar Title',
+//             style: TextStyle(
+//               letterSpacing: 2.0,
+//               wordSpacing: 4.0,
+//               color: Colors.white,
+//             ),
+//           ),
+//         ),
+//         body: Center(
+//           child: Text('My fist flutter project',
+//             style: TextStyle(
+//                 color: Colors.lightBlue,
+//                 fontSize: 20.0,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
   
-}
+// }
