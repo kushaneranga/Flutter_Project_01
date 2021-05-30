a3cf7b6fb79fabb603b41bc11c26fa79e8593509
Kushan Eranga <kem.eranga98@gmail.com>
2021-05-30 22:16:37 +0530

    device_preview 0.7.3

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// 1st Test
+
 // main() {
 //   runApp(MyApp());
 // }
@@ -12,6 +14,8 @@ import 'package:flutter/material.dart';
 //     );
 //   }
 // }
+
+//2nd Test
 
 // void main(){
 //   runApp(MyApp());
@@ -44,3 +48,38 @@ import 'package:flutter/material.dart';
 //   }
   
 // }
+
+//3rd Test
+// for device_preview 0.7.3
+import 'package:device_preview/device_preview.dart';
+
+void main() => runApp(
+    DevicePreview(
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context), // Add the locale here
+      builder: DevicePreview.appBuilder, // Add the builder here
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
