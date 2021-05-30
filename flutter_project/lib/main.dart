8a57ad7156cad14d85ae11cdc6af47303ad0dceb
Kushan Eranga <kem.eranga98@gmail.com>
2021-05-30 22:35:56 +0530

    Device Preview 0.7.3 3rd party application

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -51,35 +51,37 @@ import 'package:flutter/material.dart';
 
 //3rd Test
 // for device_preview 0.7.3
-import 'package:device_preview/device_preview.dart';
+// import 'package:device_preview/device_preview.dart';
 
-void main() => runApp(
-    DevicePreview(
-      builder: (context) => MyApp(), // Wrap your app
-    ),
-  );
+// void main() => runApp(
+//     DevicePreview(
+//       builder: (context) => MyApp(), // Wrap your app
+//     ),
+//   );
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      locale: DevicePreview.locale(context), // Add the locale here
-      builder: DevicePreview.appBuilder, // Add the builder here
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       locale: DevicePreview.locale(context), // Add the locale here
+//       builder: DevicePreview.appBuilder, // Add the builder here
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-    );
-  }
-}
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Flutter'),
+//       ),
+//     );
+//   }
+// }
