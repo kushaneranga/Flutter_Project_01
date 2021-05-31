1ce7a2d0c49fd6d556d4da4e5caa1e0ac3831677
Kushan Eranga <kem.eranga98@gmail.com>
2021-05-31 23:26:32 +0530

    5th Test

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -150,62 +150,67 @@ import 'package:flutter/material.dart';
 
 // 5th Test
 
-import 'package:device_preview/device_preview.dart';
-
-void main() => runApp(
-    DevicePreview(
-      builder: (context) => MyApp(), // Wrap your app
-    ),
-  );
+void main(){
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      locale: DevicePreview.locale(context), // Add the locale here
-      builder: DevicePreview.appBuilder, // Add the builder here
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Heading'),
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.more_horiz),
+                onPressed: () {},
+              ),
+            ],
+            flexibleSpace: Image.asset(
+                'assets/backgroundimg.jpg',
+                fit: BoxFit.cover,
+            ),
+            bottom: TabBar(
+              unselectedLabelColor: Colors.white,
+              labelColor: Colors.orange[400],
+            tabs: [
+              Tab(
+                  icon: Icon(Icons.directions_car),
+                  text: 'Kushan',
+              ),
+              Tab(icon: Icon(Icons.directions_transit)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              tab1(), // Pass widget tab1 (widget only)
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
       ),
-      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Heading'),
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {},
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(Icons.more_horiz),
-            onPressed: () {},
-          ),
-        ],
-        flexibleSpace: Image.asset(
-            'assets/backgroundimg.jpg',
-            fit: BoxFit.cover,
-        ),
-        bottom: TabBar(
-        tabs: [
-          Tab(icon: Icon(Icons.directions_car)),
-          Tab(icon: Icon(Icons.directions_transit)),
-          Tab(icon: Icon(Icons.directions_bike)),
-        ],
-      ),
-      ),
-    );
-  }
+Widget tab1() {
+  return Container(
+    child: Center(
+      child: Text('Kushan'),
+    ),
+  );
 }
