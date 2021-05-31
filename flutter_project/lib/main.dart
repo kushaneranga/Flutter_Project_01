6b2d9325af1cb80091d1b9b6039ae3c90f57303e
Kushan Eranga <kem.eranga98@gmail.com>
2021-05-31 15:54:21 +0530

    5th Test

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -147,3 +147,65 @@ import 'package:flutter/material.dart';
 //     );
 //   }
 // }
+
+// 5th Test
+
+import 'package:device_preview/device_preview.dart';
+
+void main() => runApp(
+    DevicePreview(
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context), // Add the locale here
+      builder: DevicePreview.appBuilder, // Add the builder here
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Heading'),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            onPressed: () {},
+          ),
+        ],
+        flexibleSpace: Image.asset(
+            'assets/backgroundimg.jpg',
+            fit: BoxFit.cover,
+        ),
+        bottom: TabBar(
+        tabs: [
+          Tab(icon: Icon(Icons.directions_car)),
+          Tab(icon: Icon(Icons.directions_transit)),
+          Tab(icon: Icon(Icons.directions_bike)),
+        ],
+      ),
+      ),
+    );
+  }
+}
