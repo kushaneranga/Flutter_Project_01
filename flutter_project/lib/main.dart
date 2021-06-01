5d9216a7cc16e3a12dc181274d313252bd0c5b3f
Kushan Eranga <kem.eranga98@gmail.com>
2021-06-01 11:11:23 +0530

    8th test

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -274,3 +274,42 @@ import 'package:flutter/material.dart';
 //     );
 //   }
 // }
+
+//8th Test
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.orange,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text('Kushan Eranga'),
+      ),
+      body: Container(),
+    );
+  }
+}
