43b4ad2f64a27f1a6bb0b6df87f95d57eff13e7f
Kushan Eranga <kem.eranga98@gmail.com>
2021-06-01 11:03:55 +0530

    6th Test

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -224,53 +224,53 @@ import 'package:flutter/material.dart';
 
 // 6th Test
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.orange,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Kushan Eranga'),
-      ),
-      body: Container(
-        alignment: Alignment.center, // Placement
-        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
-        // padding: EdgeInsets.all(20.0),
-        // padding: EdgeInsets.only(top: 100.0, bottom: 50.0, left: 40.0),
-        height: 200.0,
-        width: double.infinity, //for full screen size.
-        color: Colors.green,
-        transform: Matrix4.rotationZ(0.1), // Rotate.
-        child: Text(
-            'Fist flutter project',
-          style: TextStyle(
-            fontSize: 20.0,
-          ),
-        ),
-      ),
-    );
-  }
-}
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         brightness: Brightness.dark,
+//         primarySwatch: Colors.orange,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Kushan Eranga'),
+//       ),
+//       body: Container(
+//         alignment: Alignment.center, // Placement
+//         margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+//         // padding: EdgeInsets.all(20.0),
+//         // padding: EdgeInsets.only(top: 100.0, bottom: 50.0, left: 40.0),
+//         height: 200.0,
+//         width: double.infinity, //for full screen size.
+//         color: Colors.green,
+//         transform: Matrix4.rotationZ(0.1), // Rotate.
+//         child: Text(
+//             'Fist flutter project',
+//           style: TextStyle(
+//             fontSize: 20.0,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
